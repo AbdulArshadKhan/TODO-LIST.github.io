4a52530e5974179a3780f9c153f85b02c42004f1
abdul <khanarshadb1163@gmail.com>
2021-06-13 22:49:38 +0530

    Increased the font and reduced entries to 15

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-num=[17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+num=[15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
 /*let btns = document.getElementsByClassName("an")
 */
 document.getElementById("btn").addEventListener("click", ()=>
@@ -8,9 +8,8 @@ document.getElementById("btn").addEventListener("click", ()=>
 		if(a.length>0 && num.length>0)
 		{
 			newnode=document.createElement('div');
-			dp=document.createElement('p')
+			dp=document.createElement('span')
 			dp.innerHTML=a
-			//dp.style.display='inline';
 			newnode.appendChild(dp)
 			db=document.createElement('button');
 			db.innerText='X';
@@ -18,7 +17,7 @@ document.getElementById("btn").addEventListener("click", ()=>
 			db.style.float='right'
 			db.style.backgroundColor = "green";
 			var att = document.createAttribute("id");       // Create a "class" attribute
-			att.value = `a${num.pop()}`;                           // Set the value of the class attribute
+			att.value = `a${num.pop()}`;                   // Set the value of the class attribute
 			db.setAttributeNode(att);
 			
 			console.log(`The id of this button is ${db.id}`)
@@ -37,9 +36,9 @@ document.getElementById("btn").addEventListener("click", ()=>
 //console.log(document.getElementsByClassName("an").length)
 document.addEventListener('click',function(e){
     
-    for(i=1;i<18;i++)
+    for(i=1;i<16;i++)
     {
-    	if(e.target && e.target.id== `a${i}`)
+    	if(e.target && e.target.id== `a${i}`) 		// Line 37 to Line 50 are my learnings in this project
     	{
           	temp=document.getElementById(`ana${i}`);
           	num.push(i)
